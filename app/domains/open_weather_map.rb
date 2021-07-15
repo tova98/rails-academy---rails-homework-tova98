578d197983675567531f22e36c90b9366dc51f13
Nikola Tovernić <tovs98@gmail.com>
2021-07-15 15:37:17 +0200

    Fix multi city fetch method

--- app/domains/open_weather_map.rb
+++ app/domains/open_weather_map.rb
@@ -13,6 +13,11 @@ module OpenWeatherMap
   end
 
   def self.cities(city_names)
-    city_names.collect { |city_name| city(city_name) }
+    city_ids = city_names.collect { |city_name| Resolver.city_id(city_name) }.join(',')
+    multi_city_weather = (Faraday.get 'http://api.openweathermap.org/data/2.5/group',
+                                      { id: city_ids,
+                                        appid: 'd5573e972de12da9e572b97e1cd2ad98' }
+                         ).body
+    JSON.parse(multi_city_weather)['list'].map { |city| City.parse(city) }
   end
 end
