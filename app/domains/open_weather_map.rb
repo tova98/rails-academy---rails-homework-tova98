99cdf042717ab2462ef2724389c34b089a845804
Nikola Tovernić <tovs98@gmail.com>
2021-07-15 16:12:09 +0200

    Fix more bugs in cities method

--- app/domains/open_weather_map.rb
+++ app/domains/open_weather_map.rb
@@ -3,7 +3,7 @@ require 'faraday'
 module OpenWeatherMap
   def self.city(city_name)
     city_id = Resolver.city_id(city_name)
-    city_id.nil? ? return : city_id
+    return nil if city_id.nil?
 
     current_weather = (Faraday.get 'https://api.openweathermap.org/data/2.5/weather',
                                    { id: city_id,
@@ -13,11 +13,15 @@ module OpenWeatherMap
   end
 
   def self.cities(city_names)
-    city_ids = city_names.collect { |city_name| Resolver.city_id(city_name) }.join(',')
-    multi_city_weather = (Faraday.get 'http://api.openweathermap.org/data/2.5/group',
+    city_ids = city_names.collect { |city_name| Resolver.city_id(city_name) }.compact.join(',')
+    return nil if city_ids.empty?
+
+    multi_city_weather = (Faraday.get 'https://api.openweathermap.org/data/2.5/group',
                                       { id: city_ids,
                                         appid: 'd5573e972de12da9e572b97e1cd2ad98' }
                          ).body
     JSON.parse(multi_city_weather)['list'].map { |city| City.parse(city) }
   end
 end
+
+OpenWeatherMap.city('')
