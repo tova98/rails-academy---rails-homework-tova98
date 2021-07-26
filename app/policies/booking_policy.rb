64eb8e1171b873e62238513c4db9f0ec444df20e
Nikola Tovernić <tovs98@gmail.com>
2021-07-27 00:00:33 +0200

    Fix booking policy

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -2,11 +2,15 @@ class BookingPolicy < ApplicationPolicy
   def index?
     return if user.nil?
 
+    return true if record.find { |b| b.user_id == user.id }
+
     user.role == 'admin'
   end
 
   def show?
     return if user.nil?
+
+    return true if user.id == record.user_id
 
     user.role == 'admin'
   end
