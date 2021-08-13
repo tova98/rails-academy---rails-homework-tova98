18f0ea510fbfcdeadf5b45a43ad43b924aae0f0b
Nikola Tovernić <tovs98@gmail.com>
2021-08-13 20:59:04 +0200

    Undo undo changes

--- app/controllers/api/statistics/flights_controller.rb
+++ app/controllers/api/statistics/flights_controller.rb
@@ -1,8 +1,6 @@
 module Api
   module Statistics
     class FlightsController < ApplicationController
-      skip_before_action :authenticate
-
       def index
         @flights = authorize Flight.includes(:bookings).all, :statistics_index?
         render json: ::Statistics::FlightSerializer.render(@flights, root: :flights)
